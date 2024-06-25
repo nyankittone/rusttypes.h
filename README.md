20701a10349720970fe39127cf925b56bbf98e67
nyankittone <farrisluke8@gmail.com>
2024-06-25 19:18:43 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple little header file for C99+ that typedefs some datatypes to give them maybe
+more "rusty" names. For example, types like `uint8_t` are typedef'd to simply `u8`.
+
+There's also extra typedef names not from Rust. For example, the infamous `long long` type is now
+`longer`, and `unsigned int` is just `uint`.
---
+++ rusttypes.h
@@ -0,0 +1,147 @@
+// This is free and unencumbered software released into the public domain.
+// 
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+// 
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// For more information, please refer to <http://unlicense.org/>
+
+#include <stdbool.h>
+#include <stdint.h>
+
+// typedef for `long long` 
+typedef long long longer;
+
+// typedef for `unsigned int`
+typedef unsigned int uint;
+
+// typedef for `unsigned long`
+typedef unsigned long ulong;
+
+// typedef for `unsigned long long`
+typedef unsigned long long ulonger;
+
+
+// typedef for `signed int`
+typedef signed int sint;
+
+// typedef for `signed long`
+typedef signed long slong;
+
+// typedef for `signed long long`
+typedef signed long long slonger;
+
+
+// typedef for `unsigned char`
+typedef unsigned char uchar;
+
+// typedef for `signed char`
+typedef signed char schar;
+
+
+// typedef for `float`
+typedef float f32;
+
+// typedef for `double`
+typedef double f64;
+
+
+// typedef for `int8_t`
+typedef int8_t i8;
+
+// typedef for `int16_t`
+typedef int16_t i16;
+
+// typedef for `int32_t`
+typedef int32_t i32;
+
+// typedef for `int64_t`
+typedef int64_t i64;
+
+// typedef for `uint8_t`
+typedef uint8_t u8;
+
+// typedef for `uint16_t`
+typedef uint16_t u16;
+
+// typedef for `uint32_t`
+typedef uint32_t u32;
+
+// typedef for `uint64_t`
+typedef uint64_t u64;
+
+
+// typedef for `typedef`
+typedef int_least8_t i8l;
+
+// typedef for `int_least16_t`
+typedef int_least16_t i16l;
+
+// typedef for `int_least32_t`
+typedef int_least32_t i32l;
+
+// typedef for `int_least64_t`
+typedef int_least64_t i64l;
+
+// typedef for `uint_least8_t`
+typedef uint_least8_t u8l;
+
+// typedef for `uint_least16_t`
+typedef uint_least16_t u16l;
+
+// typedef for `uint_least32_t`
+typedef uint_least32_t u32l;
+
+// typedef for `uint_least64_t`
+typedef uint_least64_t u64l;
+
+
+// typedef for `typedef`
+typedef int_fast8_t i8f;
+
+// typedef for `int_fast16_t`
+typedef int_fast16_t i16f;
+
+// typedef for `int_fast32_t`
+typedef int_fast32_t i32f;
+
+// typedef for `int_fast64_t`
+typedef int_fast64_t i64f;
+
+// typedef for `uint_fast8_t`
+typedef uint_fast8_t u8f;
+
+// typedef for `uint_fast16_t`
+typedef uint_fast16_t u16f;
+
+// typedef for `uint_fast32_t`
+typedef uint_fast32_t u32f;
+
+// typedef for `uint_fast64_t`
+typedef uint_fast64_t u64f;
+
+
+// typedef for `intmax_t`
+typedef intmax_t isize;
+
+// typedef for `uintmax_t`
+typedef uintmax_t usize;
+
+
